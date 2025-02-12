8d9f0ffee5a44f6ec50854b7203990e6775f1bb4
AshwiniGaikwad3819 <ashwinigaikwad3819@gmail.com>
2025-02-12 20:00:14 +0530

    js

---
+++ Assignment.js
@@ -0,0 +1,26 @@
+//1)
+
+/*let p= 12;
+let b= 13;
+//console.log(p=b);
+//console.log(b=p);
+//b=p;
+//console.log(b);
+p=b;
+console.log(p);*/
+
+
+//2)
+/*let a=23;
+let b=25;
+console.log(a+=b);
+console.log(a-=b);
+console.log(a*=b);
+console.log(a/=b);*/
+
+//3)
+
+let a= 65835;
+ let b=3288;
+ console.log(a%=b);
+
