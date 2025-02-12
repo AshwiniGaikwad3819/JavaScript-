3b3944107b27eec1a65c05f17c29ce049f572b88
AshwiniGaikwad3819 <ashwinigaikwad3819@gmail.com>
2025-02-12 20:02:44 +0530

    js

---
+++ unarry.js
@@ -0,0 +1,18 @@
+/*let a = 300;
+let b=29;
+//console.log(a++);
+//console.log(b++);
+console.log(++a);
+console.log(++b);*/
+
+//2)
+    let a=245;
+    console.log(a++);
+    console.log(++a);
+    console.log(+a);
+
+//3)
+     let k=12;
+     console.log(--k);
+     //console.log(k--);
+
